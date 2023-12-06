38f4c5626c9ddfbc495848abf9ab1caafd3aa66a
Dipak Roy <rdipak109@gmail.com>
2023-12-06 10:40:07 +0600

    update models

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+@admin.register(models.Student)
+class Studentadmin(admin.ModelAdmin):
+    list_display = ['name','roll','department','phone']
